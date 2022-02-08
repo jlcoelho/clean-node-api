85ddf3a62bb8b8caec29056bd77e0848fad0416c
João Lucas <lucas_0423@hotmail.com>
2022-02-07 23:27:35 -0300

    feat: ensure DbAddAccount returns an account on success

--- src/data/protocols/add-account-repository.ts
+++ src/data/protocols/add-account-repository.ts
@@ -2,5 +2,5 @@ import { AddAccountModel } from '../../domain/useCases/add-account'
 import { AccountModel } from '../../domain/models/account'
 
 export interface AddAccountRepository {
-  add: (accountData: AddAccountModel) => Promise<AccountModel | null>
+  add: (accountData: AddAccountModel) => Promise<AccountModel>
 }
