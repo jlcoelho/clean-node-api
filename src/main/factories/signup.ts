8ac8a2e15b88d478b5cca7c1517124f7ab9d9fb3
João Lucas <lucas_0423@hotmail.com>
2022-03-05 12:14:15 -0300

    refactor: move EmailValidation to the ValidationComposite

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,5 +1,4 @@
 import { SignUpController } from '../../presentation/controllers/signup/signup'
-import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { DbAddAccount } from '../../data/useCases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptografhy/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
@@ -11,12 +10,11 @@ import { makeSignUpValidation } from './signup-validation'
 
 export const makeSignUpController = (): Controller => {
   const salt = 12
-  const emailValidatorAdapter = new EmailValidatorAdapter()
   const accountMongoRepository = new AccountMongoRepository()
   const bcryptAdapter = new BcryptAdapter(salt)
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
   const validationComposite = makeSignUpValidation()
-  const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount, validationComposite)
+  const signUpController = new SignUpController(dbAddAccount, validationComposite)
   const logMongoRepository = new LogMongoRepository()
   return new LogControllerDecorator(signUpController, logMongoRepository)
 }
