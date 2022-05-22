d6934d55b73b3fb6bf1411b47f97ea2aa0a9314c
João Lucas <lucas_0423@hotmail.com>
2022-05-22 00:59:38 -0300

    feat: ensure DbAuthentication calls HashComparer with correct values

--- src/data/protocols/db/load-account-by-email-repository.ts
+++ src/data/protocols/db/load-account-by-email-repository.ts
@@ -1,5 +1,5 @@
 import { AccountModel } from '../../useCases/add-account/db-add-account-protocols'
 
 export interface LoadAccountByEmailRepository {
-  load: (email: string) => Promise <AccountModel | null>
+  load: (email: string) => Promise <AccountModel>
 }
